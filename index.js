8e0bec8f54463f1b652761a8f671b68d10cdce20
Nifi <heyitsnifi@gmail.com>
2023-05-28 22:00:10 +0530

    Get user deposit and number of lines to bet

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+//-------------------A simple JS program to create a slot machine------------------------
 //Get user deposit
+//Determine the no of lines to bet
+//Collect a bet amount
+//Spin the slot machine
+//Check if user won
+//Gift the user their winning
+//Play again
+
+
+const prompt = require("prompt-sync")();
+
+//collect the deposit from user
+const deposit = () => {
+
+    while(true){
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+        if(isNaN(numberDepositAmount) || numberDepositAmount<=0){
+            console.log("Invalid amount, try again.");
+        }
+        else{
+            return numberDepositAmount;
+        }
+    }
+
+}
+
+//collect the no of line they wanna bet
+const getNumberOfLines = () => {
+
+    while(true){
+    const lines = prompt("Enter the number of lines you want to bet: ")
+    const numberOfLines = parseFloat(depositAmount);
+
+        if(isNaN(numberOfLines) || numberOfLines<=0 || numberOfLines>3){
+            console.log("Invalid number of lines, try again.");
+        }
+        else{
+            return numberOfLines;
+        }
+    }
+
+}
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
